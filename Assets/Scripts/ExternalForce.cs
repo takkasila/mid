eb2362cd8a443c3da996d37c8c31a71c24e72235
Takkasila Saichol <takkasila@outlook.com>
2016-03-09 23:58:48 +0700

    Added ForceField and ForceVortex

---
+++ Assets/Scripts/ExternalForce.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExternalForce : MonoBehaviour {
+
+    Rigidbody2D selfRb;
+    void Start()
+    {
+        selfRb = GetComponent<Rigidbody2D>();
+    }
+
+    public void AddForce(Vector2 dir, float amount)
+    {
+        selfRb.AddForce(dir.normalized * amount, ForceMode2D.Force);
+    }
+}
---
+++ Assets/Scripts/ForceField.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceField : MonoBehaviour {
+
+    public Vector2 ForceDir;
+    public float ForceAmount;
+
+    void Start()
+    {
+        ForceDir = ForceDir.normalized;
+    }
+	void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.tag=="Movable")
+        {
+            col.gameObject.GetComponent<ExternalForce>().AddForce(ForceDir, ForceAmount);
+        }
+    }
+
+}
