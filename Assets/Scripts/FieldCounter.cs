b3c0f666119673866aa352120b1c3aa8c279802a
Takkasila Saichol <takkasila@outlook.com>
2016-03-10 17:22:31 +0700

    Added count field

---
+++ Assets/Scripts/FieldCounter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldCounter : MonoBehaviour {
+
+    public bool isTriggerAtFinish;
+    public GameObject TargetGameObject;
+    public string TriggerFunction;
+    public int ThresholdToTrigger;
+
+    public int counter = 0;
+
+    void Start()
+    {
+
+    }
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        counter++;
+
+        if(isTriggerAtFinish)
+        {
+            if(counter >=ThresholdToTrigger)
+            {
+                TargetGameObject.SendMessage(TriggerFunction, SendMessageOptions.RequireReceiver);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/FieldCounterEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(FieldCounter))]
+public class FieldCounterEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        FieldCounter editor = (FieldCounter)target;
+
+        editor.isTriggerAtFinish = EditorGUILayout.Toggle("Trigger Event at Finish", editor.isTriggerAtFinish);
+
+        if(editor.isTriggerAtFinish)
+        {
+            editor.TargetGameObject = EditorGUILayout.ObjectField("Target Game Object", editor.TargetGameObject, typeof(GameObject)) as GameObject;
+            editor.TriggerFunction = EditorGUILayout.TextField("Function name", editor.TriggerFunction);
+            editor.ThresholdToTrigger = EditorGUILayout.IntField("Threshold to trigger", editor.ThresholdToTrigger);
+        }
+
+        EditorGUILayout.TextArea("Count: " + editor.counter.ToString());
+    }
+}
