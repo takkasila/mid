dd6de45560095e461340ccee21e73c088b793c86
Takkasila Saichol <takkasila@outlook.com>
2016-05-27 20:07:59 +0700

    Score sync

---
+++ Assets/CatFieldCounter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatFieldCounter : MonoBehaviour {
+
+    public ScoreManager score;
+    public int side;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Cat")
+        {
+            Debug.Log("Hit cat!");
+            score.IncreaseScore(side);
+        }
+    }
+}
---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+    public Text LeftField, RightField;
+
+    public void IncreaseScore(int side)
+    {
+        switch(side)
+        {
+            case 0:
+                LeftField.text = (int.Parse(LeftField.text) + 1).ToString();
+                break;
+            case 1:
+                RightField.text = (int.Parse(LeftField.text) + 1).ToString();
+                break;
+        }
+    }
+}
