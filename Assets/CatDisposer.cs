378f92084945ff7dd9a558f55e678274e6f3f692
Takkasila Saichol <takkasila@outlook.com>
2016-05-27 16:18:00 +0700

    Polising level

---
+++ Assets/CatDisposer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class CatDisposer : NetworkBehaviour {
+
+    public GameObject cat;
+    public float DisposeInterval;
+    float counter;
+	void Start () {
+        counter = DisposeInterval;
+	}
+	
+	void Update () {
+        counter -= Time.deltaTime;
+
+        if(counter <=0)
+        {
+            counter = DisposeInterval;
+            GameObject newCat = (GameObject)Instantiate(cat);
+            newCat.transform.position = transform.position;
+            NetworkServer.Spawn(newCat);
+        }
+	
+	}
+}
